7fe32ad0cd98db031aabf68a1d7c6ba5bc555458
Naftali-B <nbudamba@gmail.com>
2023-05-03 11:51:41 -0700

    regular expressions

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,7 +1,4 @@
 #!/usr/bin/env ruby
-
-# Get the argument passed in from the command line
-# Matches "School"
-# If the argument matches the pattern, print each occurrence of "School"
+# Get the argument passed in from the command line and Matches with "School"
 
 puts ARGV[0].scan(/School/).join

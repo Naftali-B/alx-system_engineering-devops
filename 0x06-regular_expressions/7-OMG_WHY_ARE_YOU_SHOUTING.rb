b2562995cead22fcd156520691f36f863170e66f
Naftali-B <nbudamba@gmail.com>
2023-05-04 02:53:11 -0700

    regular expressions

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+# Get the argument passed in from the command line and matches it with the case(s)
+# If the argument matches the pattern, print results
+
+puts ARGV[0].scan(/[A-Z]+/).join
+

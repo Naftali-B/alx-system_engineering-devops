71da787d9eef94afab971b4cf7d9adc60a052a7d
Naftali-B <nbudamba@gmail.com>
2023-05-04 01:17:55 -0700

    regular expressions

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -3,5 +3,5 @@
 # Get the argument passed in from the command line and matches it with the case(s)
 # If the argument matches the pattern, print results
 
-puts ARGV[0].scan(/hbt{1,4}n/)
+puts ARGV[0].scan(/hbt{1,4}n/).join
 

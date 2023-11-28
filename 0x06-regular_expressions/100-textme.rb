fa3f2f5e343293c94109c4179016c7f254e8a29d
Naftali-B <nbudamba@gmail.com>
2023-11-28 20:47:56 +0300

    regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,6 +2,6 @@
 # Your script should output: [SENDER],[RECEIVER],[FLAGS]
 # The sender phone number or name (including country code if present)
 # The receiver phone number or name (including country code if present)
-# The flags that were used
+# The flags that were used.
 
 puts ARGV[0].scan(/\[from:(\w+|+\d{11,11})\]\s\[to:(\w+|+\d{10,10})\]\s\[flags:-?\d:-?\d:-?\d:-?\d:-?\d\]/).join

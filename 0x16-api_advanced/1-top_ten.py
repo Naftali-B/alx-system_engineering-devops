2d9658de8c8da6cba1d9f3de2c124352d3358671
Naftali-B <nbudamba@gmail.com>
2024-03-06 01:04:43 +0300

    advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,13 +2,16 @@
 '''queries the Reddit API and prints the titles of the first 10 hot posts'''
 import requests
 
+
 def top_ten(subreddit):
-    '''prints the titles of the first 10 hot posts listed for a given subreddit'''
+    '''prints the titles of the first 10 hot posts
+    listed for a given subreddit'''
     if subreddit is None or not isinstance(subreddit, str):
         print(None)
         return
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    res = requests.get(url, headers={'User-Agent': 'Particular-Demand377'}).json()
+    res = requests.get(url, headers={
+                    'User-Agent': 'Particular-Demand377'}).json()
     if 'error' in res.keys():
         print(None)
         return
